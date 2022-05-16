f65b25a00794bec04fa2720ccb0195b83232e588
Tokiopro <tokisaonana@yahoo.co.jp>
2022-05-16 07:13:55 +0000

    ログイン画面、topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root to: 'homes#top'
+  end
   #管理者用
   #URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
