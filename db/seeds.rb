a6b421f4ae7af6bbf5196d55393782edb6b3f563
Tokiopro <tokisaonana@yahoo.co.jp>
2022-05-16 08:45:44 +0000

    ログイン後、トップページ遷移

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(email: 'hatatokio00@gmail.com', password: 'appledwc')
