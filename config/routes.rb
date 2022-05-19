f8d1ab086eeea078138236537d82f95d1b333797
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2022-05-19 07:12:28 +0000

    ENDUSER[FINISH]

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,25 @@ Rails.application.routes.draw do
 
   #顧客用
   #URL /end_users/sign_in ...
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about", as: "about"
+    resource :end_users, only: [:edit, :show, :update]
+    # 退会確認画面
+    get '/end_users/:id/unsubscribe' => 'end_users#unsubscribe', as: 'unsubscribe'
+    # 論理削除用のルーティング
+    patch '/end_users/:id/withdrawal' => 'end_users#withdrawal', as: 'withdrawal'
+  end
+
+  scope module: :public do
+    resources :items, only: [:index, :show]
+  end
+
   devise_for :end_users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
 
-  scope module: :public do
-    root to: "homes#top"
-    get "about" => "homes#about", as: "about"
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
