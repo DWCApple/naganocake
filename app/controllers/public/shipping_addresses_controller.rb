5f3146224231de0adac5797b98493838bad2390f
Tokiopro <tokisaonana@yahoo.co.jp>
2022-05-21 03:59:29 +0000

    配送先住所登録、編集画面

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -3,19 +3,36 @@ class Public::ShippingAddressesController < ApplicationController
     @shipping_addresses = ShippingAddress.all
     @new_address = ShippingAddress.new
   end
-  
+
   def create
-    @shipping_address = ShippingAddress.new(shipping_address_params)
-    if @shipping_address.save
+    @new_address = ShippingAddress.new(shipping_address_params)
+    if @new_address.save
       redirect_to shipping_addresses_path , notice: "配送先を登録しました"
     else
+      @shipping_addresses = ShippingAddress.all
       render :index
     end
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
-  
+
+  def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    if @shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path, notice: "配送先の情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @shipping_address = ShippingAddress.find(params[:id])
+    @shipping_address.destroy
+    redirect_to shipping_addresses_path, notice: "配送先を削除しました"
+  end
+
   private
   def shipping_address_params
     params.require(:shipping_address).permit(:name, :address, :postal_code)
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -5,5 +5,4 @@ class ShippingAddress < ApplicationRecord
   #正規表現で都道府県、市町村を制限
   VARIDATE_ADDRESS = /(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])(.+)/
   validates :address, presence: true, uniqueness: true, format: { with: VARIDATE_ADDRESS }
-
 end
