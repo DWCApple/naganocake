676df6b31473d8e0e82f38db664763ea0d04c576
kenbow98 <kenbow.998@gmail.com>
2022-05-25 08:50:04 +0000

    更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,7 +9,7 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @cart_items = current_end_user.cart_items.all
     @total_price = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
-    # @billing_amount = @total_price + @order.postages
+    @billing_amount = @total_price + @order.postages
     if params[:order][:order_addresses] == '0'
       @order.postal_code = current_end_user.postal_code
       @order.address = current_end_user.address
@@ -31,34 +31,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @cart_items = current_end_user.cart_items.all
-    p @order = current_end_user.orders.new(order_params)
-    if @order.save
-      @cart_items.each do |cart|
-      order_detail = OrderDetail.new
-        order_detail.item_id = cart.item_id
-        order_detail.order_id = @order.id
-        order_detail.item_quantity = cart.item_quantity
-        order_detail.price = cart.item.add_tax_price
-        order_detail.save
-      end
-      current_end_user.cart_items.destroy_all
-      redirect_to thanks_path
-    else
-      @order = Order.new(order_params)
-      render 'new'
-    end
-
   end
 
   def index
-    @orders = Order.where(end_user_id: current_end_user.id)
   end
 
   def show
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details
-    @total_price = 0
   end
 
   private
