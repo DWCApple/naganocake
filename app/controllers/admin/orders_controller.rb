39da46ca01ac31da1064a2e96cdb84a3bd24374a
Tokiopro <tokisaonana@yahoo.co.jp>
2022-05-28 05:32:16 +0000

    注文履歴実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,16 +1,19 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_status = Order.new(order_params)
+    @order_items = @order.order_items
   end
-  
+
   def update
-    @orders = Order.new(order_params)
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order.id), notice: '更新に成功しました'
+    end
   end
-  
+
   private
-  
+
   def order_params
-    params.require(:orders).permit(:status)
+    params.require(:order).permit(:status, :order_items_id, :end_users_id)
   end
 end
